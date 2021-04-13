4b3e1d685462e69da3cfaf828ad7046367da80e5
Guru-Eco <abhimanyu768692@gmail.com>
2021-04-13 15:15:07 +0530

    FooterUi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Heading } from "@chakra-ui/layout";
+import { Footer } from "./Shared-components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Heading>Milaap Clone</Heading>
+      <Footer />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <BrowserRouter>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
