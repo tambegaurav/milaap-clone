36b74b8964c5cd9d464ca45095f1b6bcb0c9865a
tambegaurav <gauravkishortambe@gmail.com>
2021-04-13 12:05:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Heading } from "@chakra-ui/layout";
+
+function App() {
+  return (
+    <div className="App">
+      <Heading>Milaap Clone</Heading>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import { Heading } from "@chakra-ui/layout";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Heading>Home</Heading>
+    </div>
+  );
+};
+
+export default HomePage;
