6c7adadddc16e5ef006a0750a9fd85e8a9d7881a
manish7377 <manish7377873265@gmail.com>
2021-04-13 16:46:30 +0530

    navbar

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,9 +1,11 @@
 import { Heading } from "@chakra-ui/layout";
 import React from "react";
+import { Navbar } from "../../Shared-components/Navbar";
 
 const HomePage = () => {
   return (
     <div>
+      <Navbar/>
       <Heading>Home Page</Heading>
     </div>
   );
